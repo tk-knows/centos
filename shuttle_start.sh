24f6f8cc337a94d74dc2bf5fd8cee2b8d7938408
tk periasamy <tperiasa@cisco.com>
2020-06-08 05:33:02 -0500

    added options " -N --dns  -x 172.17.0.0/16" to sshuttle command

--- shuttle_start.sh
+++ shuttle_start.sh
@@ -3,7 +3,7 @@
 # disable timeout
 set timeout -1
 
-spawn sshuttle -l 0.0.0.0  -r virl@10.81.59.228 -D 172.16.1.0/24
+spawn sshuttle -l 0.0.0.0  -N --dns -r virl@10.81.59.228 -D 172.16.1.0/24 -x 172.17.0.0/16
 expect "virl@10.81.59.228's password: "
 send -- "VIRL\r"
 
